f34c4815edc980abf34d2206bd106ecca69cbbfb
Robert <robethuku11@gmail.com>
2022-12-04 22:07:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded",showNewElement);
+
+
+function showNewElement() {
+  let intialParagraph = document.querySelector('p');
+  let updatedParagraph = document.createElement('p');
+  updatedParagraph.id = "text";
+  updatedParagraph.innerHTML = "This is really cool!"
+  document.body.replaceChild(updatedParagraph,intialParagraph);
+
+}
